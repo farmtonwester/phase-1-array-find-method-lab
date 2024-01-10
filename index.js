16b21641ff5c2240207d7f6aa1c7ece66f2b2d7b
Weston Farmer <wesfarmer11@gmail.com>
2024-01-10 15:30:24 -0600

    both tests pass

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(record) {
+    const yearOfWin = record.find((element) => element.result === "W") 
+            if (yearOfWin){
+                return yearOfWin.year
+            }
+            // else {
+            //     return undefined
+            // }
+        }
+    
+        
+// if the callback function returns a truthy value, we want to return yearOfWin.year
+// if the callback function returns undefined, we want to simply return undefined
+
+// right now i can get one test passing or the other, but not both
+
+
